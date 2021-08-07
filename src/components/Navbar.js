183f7c13c68b3677d36d881898c0c52f582d6740
fatihccetin <fatih.ccetin2@gmail.com>
2021-08-08 02:50:19 +0300

    video

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom'
+
+
+function Navbar() {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+
+                        <Link to="/" className="navbar-logo">
+                            TRVL
+                        </Link>
+
+                </div>
+
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
